3716fd69b827075f9741e8f54606fde0818fc395
RALPH22222 <ralphmonzales665@gmail.com>
2026-02-08 00:27:34 +0800

    first commit -Jee-ps

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <h1 className="text-3xl text-amber-300 font-bold underline">
+          Hello Jee-ps
+          </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
