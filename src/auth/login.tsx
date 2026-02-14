d7b98b120df3dc1b202aaea055cc13545a102c62
RALPH22222 <ralphmonzales665@gmail.com>
2026-02-14 09:05:25 +0800

    fix endpoints

--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -16,7 +16,7 @@ export default function Login() {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:3000/auth/login', {
+            const response = await fetch('https://jee-ps-server.onrender.com/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
